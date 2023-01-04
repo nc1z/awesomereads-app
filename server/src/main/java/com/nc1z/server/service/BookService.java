b869904c6e0a7785439a19a3c0ca52222c439a23
nc1z <nc1zdev@gmail.com>
2023-01-04 14:17:20 +0800

    [BookService] Added currentLoansCount method

--- server/src/main/java/com/nc1z/server/service/BookService.java
+++ server/src/main/java/com/nc1z/server/service/BookService.java
@@ -54,4 +54,8 @@ public class BookService {
             return false;
         }
     }
+
+    public int currentLoansCount(String userEmail) {
+        return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
 }
