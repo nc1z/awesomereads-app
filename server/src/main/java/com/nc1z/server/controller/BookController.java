8281621b4a7305b86c82da8dbb3f2230688b6316
nc1z <nc1zdev@gmail.com>
2023-01-04 17:40:17 +0800

    [BookController] Refactor to extract JWT payload from Authorization headers

--- server/src/main/java/com/nc1z/server/controller/BookController.java
+++ server/src/main/java/com/nc1z/server/controller/BookController.java
@@ -14,21 +14,21 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
         // TODO: Temp hardcoded email for testing, Replace this with extracted email
         String userEmail = "testuser@email.com";
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
         // TODO: Temp hardcoded email for testing, Replace this with extracted email
         String userEmail = "testuser@email.com";
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
         // TODO: Temp hardcoded email for testing, Replace this with extracted email
         String userEmail = "testuser@email.com";
         return bookService.checkoutBook(userEmail, bookId);
