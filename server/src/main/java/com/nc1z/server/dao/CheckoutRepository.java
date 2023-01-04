b18d2e030073c689042e916c7a5d894fa9ddb662
nc1z <nc1zdev@gmail.com>
2023-01-04 14:14:16 +0800

    [CheckoutRepository] Added new interface method to find books by userEmail

--- server/src/main/java/com/nc1z/server/dao/CheckoutRepository.java
+++ server/src/main/java/com/nc1z/server/dao/CheckoutRepository.java
@@ -3,7 +3,11 @@ package com.nc1z.server.dao;
 import com.nc1z.server.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
