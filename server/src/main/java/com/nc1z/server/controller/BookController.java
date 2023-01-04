ad604fa721ae61f9288a68982ddb7a5ed16ed591
nc1z <nc1zdev@gmail.com>
2023-01-04 13:46:58 +0800

    [Controller] Added BookController with PUT endpoint for checkoutBook

---
+++ server/src/main/java/com/nc1z/server/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.nc1z.server.controller;
+
+import com.nc1z.server.entity.Book;
+import com.nc1z.server.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        // TODO: Temp hardcoded email for testing, Replace this with extracted email
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
