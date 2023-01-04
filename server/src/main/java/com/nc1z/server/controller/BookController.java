0cf7a23ed69573eb7296ee30d31a2222cca3c80c
nc1z <nc1zdev@gmail.com>
2023-01-04 18:10:18 +0800

    [BookController] Added payloadJWTExtraction Method to get userEmail

--- server/src/main/java/com/nc1z/server/controller/BookController.java
+++ server/src/main/java/com/nc1z/server/controller/BookController.java
@@ -2,6 +2,7 @@ package com.nc1z.server.controller;
 
 import com.nc1z.server.entity.Book;
 import com.nc1z.server.service.BookService;
+import com.nc1z.server.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,22 +16,19 @@ public class BookController {
 
     @GetMapping("/secure/currentloans/count")
     public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
-        // TODO: Temp hardcoded email for testing, Replace this with extracted email
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJWT.payloadJWTExtraction(token);
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
     public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
-        // TODO: Temp hardcoded email for testing, Replace this with extracted email
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJWT.payloadJWTExtraction(token);
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
     public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
-        // TODO: Temp hardcoded email for testing, Replace this with extracted email
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJWT.payloadJWTExtraction(token);
         return bookService.checkoutBook(userEmail, bookId);
     }
 }
