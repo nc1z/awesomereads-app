70608cb556f3ff459756dcd237748349bf5a0c49
nc1z <nc1zdev@gmail.com>
2023-01-05 15:11:35 +0800

    [carousel] Carousel setup, logic, styles

--- client/src/components/Homepage/BookCarousel.tsx
+++ client/src/components/Homepage/BookCarousel.tsx
@@ -1,16 +1,101 @@
-import axios from "axios";
 import React from "react";
+import BookModel from "../../models/BookModel";
+import Carousel from "react-bootstrap/Carousel";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const BookCarousel = () => {
-  const fetchBooks = async () => {
-    try {
-      const { data: response } = await axios.get("/api/books");
-      console.log(response);
-    } catch (error: any) {
-      console.log(error.message);
+interface BookCarouselProps {
+  books: BookModel[];
+}
+
+const CarouselHolder = styled(Carousel)`
+  //   border: 5px solid lime;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const CarouselItemDiv = styled.div`
+  //   border: 5px solid yellow;
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 480px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+const BookImage = styled.img`
+  margin: 2rem 0;
+  width: 20%;
+  box-shadow: rgba(0, 0, 0, 0.35) 1.95px 1.95px 1.95px 2.6px;
+
+  @media (max-width: 480px) {
+    width: 50%;
+  }
+`;
+
+const CaptionDiv = styled.div`
+  display: flex;
+  flex-direction: Column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  max-width: 30vw;
+
+  h1 {
+    font-size: 3.5rem;
+    font-weight: 900;
+  }
+
+  p {
+    font-weight: 500;
+  }
+
+  @media (max-width: 480px) {
+    h1,
+    p {
+      display: none;
     }
-  };
-  return <div onClick={fetchBooks}>Carousel</div>;
+  }
+`;
+
+const ReserveButton = styled.button`
+  a {
+    color: var(--main-white);
+  }
+  &:hover {
+    background-color: var(--main-red);
+  }
+`;
+
+const BookCarousel = ({ books }: BookCarouselProps) => {
+  return (
+    <CarouselHolder>
+      {books.map((book) => (
+        <Carousel.Item key={book.id}>
+          <CarouselItemDiv>
+            <BookImage
+              src={book.img ? book.img : "../../public/vite.svg"}
+              alt="Second slide"
+            />
+            <CaptionDiv>
+              <h1>{book.title}.</h1>
+              <p>{book.author}</p>
+              <ReserveButton>
+                <Link to="/">Reserve</Link>
+              </ReserveButton>
+            </CaptionDiv>
+          </CarouselItemDiv>
+        </Carousel.Item>
+      ))}
+    </CarouselHolder>
+  );
 };
 
 export default BookCarousel;
---
+++ client/src/components/Homepage/CarouselContainer.tsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import ErrorDiv from "../Error/ErrorDiv";
+import Loading from "../Loading/Loading";
+import BookCarousel from "./BookCarousel";
+
+const CarouselContainer = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchBooks = async () => {
+    try {
+      const { data: response } = await axios.get("/api/books?page=0&size=10");
+
+      if (!response._embedded.books) {
+        return setErrorMessage("No books found. Something went wrong.");
+      }
+
+      const responseData = response._embedded.books;
+      const booksArray: BookModel[] = responseData.map((book: any) => {
+        return {
+          id: book.id,
+          title: book.title,
+          author: book.author,
+          description: book.description,
+          copies: book.copies,
+          copiesAvailable: book.copiesAvailable,
+          category: book.category,
+          img: book.img,
+        };
+      });
+
+      setBooks(booksArray);
+      setIsLoading(false);
+      console.log(response);
+    } catch (error: any) {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="container m-4">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+      <div className="container m-4">
+        <ErrorDiv errorMessage={errorMessage} />
+      </div>
+    );
+  }
+  return <BookCarousel books={books} />;
+};
+
+export default CarouselContainer;
