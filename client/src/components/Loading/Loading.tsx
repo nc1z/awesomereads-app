5f954dcbcb9479060dffaf19bf5fea42128e8b5f
nc1z <nc1zdev@gmail.com>
2023-01-05 12:26:55 +0800

    [Loading] Added global loading component

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import styled from "styled-components";
+
+const LoadingBar = styled(ProgressBar)`
+  background: var(--main-white) !important;
+  max-width: 30vw;
+`;
+
+const Loading = () => {
+  const [progress, setProgress] = useState(45);
+
+  useEffect(() => {
+    setTimeout(() => setProgress(85), 3000);
+  }, []);
+
+  return <LoadingBar variant="warning" striped animated now={progress} />;
+};
+
+export default Loading;
