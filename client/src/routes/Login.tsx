ba718de087fac065cd3c0797d27e4c45b4e86cc9
nc1z <nc1zdev@gmail.com>
2023-01-05 17:09:24 +0800

    Added Container

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+  //   border: 10px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 20%;
+`;
 
 const Login = () => {
-  return <div>Login</div>;
+  return <LoginContainer>Login</LoginContainer>;
 };
 
 export default Login;
--- client/src/routes/Search.tsx
+++ client/src/routes/Search.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const SearchContainer = styled.div`
+  //   border: 10px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 20%;
+`;
 
 const Search = () => {
-  return <div>Search</div>;
+  return <SearchContainer>Search</SearchContainer>;
 };
 
 export default Search;
