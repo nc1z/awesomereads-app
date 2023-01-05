7ece871f339fa5ebe9091d050702f07a715b0f2e
nc1z <nc1zdev@gmail.com>
2023-01-05 13:42:16 +0800

    [Home] Updated HomeContainer style

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import CarouselContainer from "../components/Homepage/CarouselContainer";
+
+const HomeContainer = styled.div`
+  border: 5px solid red;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 20%;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <HomeContainer>
       <CarouselContainer />
-    </div>
+    </HomeContainer>
   );
 };
 
