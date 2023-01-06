9bffbfeed42b0ac31a785a84678611ff1386329f
nc1z <nc1zdev@gmail.com>
2023-01-06 13:42:06 +0800

    [Book] Updated Book

--- client/src/routes/Book.tsx
+++ client/src/routes/Book.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import BookPage from "../components/Book/BookPage";
+
+const BookRouteContainer = styled.div`
+  // border: 5px solid red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 20%;
+`;
 
 const Book = () => {
-  return <div>Book</div>;
+  return (
+    <BookRouteContainer>
+      <BookPage />
+    </BookRouteContainer>
+  );
 };
 
 export default Book;
