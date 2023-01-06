e8c8f5e797d40cb1fe2b02d1d8caab46f7449bf8
nc1z <nc1zdev@gmail.com>
2023-01-06 16:03:55 +0800

    [BookPage] Checkout css refactor

--- client/src/components/Book/BookCheckout.tsx
+++ client/src/components/Book/BookCheckout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const BookCheckoutDiv = styled.div`
-  border: 5px solid black;
+  // border: 5px solid black;
+  padding: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   display: flex;
   flex-direction: column;
   font-size: 1rem;
