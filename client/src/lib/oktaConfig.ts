0a1a8ddb24ae036adf8751738be1219299e0e6ce
nc1z <nc1zdev@gmail.com>
2023-01-07 21:50:50 +0800

    [Auth] Updated oktaConfig

--- client/src/lib/oktaConfig.ts
+++ client/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: import.meta.env.VITE_OKTA_CLIENTID,
   issuer: `https://${import.meta.env.VITE_OKTA_ISSUER}/oauth2/default`,
-  redirectUri: "http://localhost:3000/login/callback",
+  redirectUri: `${window.location.origin}/login/callback`,
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
