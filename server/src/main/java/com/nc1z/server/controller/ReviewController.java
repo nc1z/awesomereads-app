751b872ab9864fe9a089101a9c69ed4ad908feed
nc1z <nc1zdev@gmail.com>
2023-01-08 14:15:08 +0800

    [ReviewController] Added GET mapping to check user book reviewed state

--- server/src/main/java/com/nc1z/server/controller/ReviewController.java
+++ server/src/main/java/com/nc1z/server/controller/ReviewController.java
@@ -14,6 +14,16 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @GetMapping("/secure/user/book")
+    public Boolean reviewBookByUser(@RequestHeader(value = "Authorization") String token,
+                                    @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
+
     @PostMapping("/secure")
     public void postReview(@RequestHeader(value = "Authorization") String token,
                            @RequestBody ReviewRequest reviewRequest) throws Exception {
