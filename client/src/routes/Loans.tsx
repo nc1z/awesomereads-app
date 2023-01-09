a40cd5f783d2c43e15d444be3d26f9a4c49b1349
nc1z <nc1zdev@gmail.com>
2023-01-09 10:08:11 +0800

    [Loans] Created route & components

---
+++ client/src/routes/Loans.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoansRouteContainer = styled.div`
+  // border: 5px solid red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loans = () => {
+  return <LoansRouteContainer>Loans</LoansRouteContainer>;
+};
+
+export default Loans;
