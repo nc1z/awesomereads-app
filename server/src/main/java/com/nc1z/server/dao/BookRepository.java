9e6e688012be27a22a7fc0f19fdf97f75bc1ffc1
nc1z <nc1zdev@gmail.com>
2023-01-10 10:36:36 +0800

    [BookRepository] Added Query findBooksByBookIds

--- server/src/main/java/com/nc1z/server/dao/BookRepository.java
+++ server/src/main/java/com/nc1z/server/dao/BookRepository.java
@@ -4,9 +4,16 @@ import com.nc1z.server.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    @Query("select b from Book b where id in :book_ids")
+    List<Book> findBooksByBookIds(@Param("book_ids") List<Long> bookId);
 }
