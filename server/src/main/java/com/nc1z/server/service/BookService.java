3c791f8ed1777c7a9272e8ec272656e9f4e86b99
nc1z <nc1zdev@gmail.com>
2023-01-11 14:15:21 +0800

    [BookService] Added returnBook method

--- server/src/main/java/com/nc1z/server/service/BookService.java
+++ server/src/main/java/com/nc1z/server/service/BookService.java
@@ -109,4 +109,20 @@ public class BookService {
 
         return currentLoansResponses;
     }
+
+    public void returnBook (String userEmail, Long bookId) throws Exception {
+
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (!book.isPresent() || validateCheckout == null) {
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+
+        bookRepository.save(book.get());
+        checkoutRepository.deleteById(validateCheckout.getId());
+    }
 }
