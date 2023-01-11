7f347128a66fe6250ef9cc5c18be5a9755a8ed50
nc1z <nc1zdev@gmail.com>
2023-01-11 12:14:30 +0800

    [BooksDisplay] Refactor StyledLink

--- client/src/components/Search/BooksDisplay.tsx
+++ client/src/components/Search/BooksDisplay.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import styled from "styled-components";
 import BookModel from "../../models/BookModel";
+import { StyledLink } from "../../Utils/StyledExports";
 
 interface BooksDisplayProps {
   books: BookModel[];
@@ -44,26 +44,6 @@ const BookImg = styled.img`
   }
 `;
 
-const BookLink = styled(Link)`
-  font-size: 12px;
-  color: var(--main-white);
-  background-color: var(--main-orange);
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.4em 1.2em;
-  font-family: inherit;
-  cursor: pointer;
-
-  &:hover {
-    color: var(--main-white);
-    background-color: var(--main-red);
-  }
-  &:active {
-    color: var(--main-white);
-    background-color: var(--main-red);
-  }
-`;
-
 const BookTitleHeader = styled.h3`
   display: flex;
   align-items: center;
@@ -86,7 +66,7 @@ const BooksDisplay = ({ books }: BooksDisplayProps) => {
             <span>{book.author}</span>
             <BookTitleHeader>
               {book.title}
-              <BookLink to={`/book/${book.id}`}>Learn more</BookLink>
+              <StyledLink to={`/book/${book.id}`}>Learn more</StyledLink>
             </BookTitleHeader>
             <p>{book.description}</p>
           </BookDiv>
