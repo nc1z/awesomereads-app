f05fb97a169e8916702e0f4e6a608deea65082fd
nc1z <nc1zdev@gmail.com>
2023-01-13 10:04:09 +0800

    [Service] Added MessagesService & Injected MessageRepository

---
+++ server/src/main/java/com/nc1z/server/service/MessagesService.java
@@ -0,0 +1,21 @@
+package com.nc1z.server.service;
+
+import com.nc1z.server.dao.MessageRepository;
+import com.nc1z.server.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+}
