e2f9b65155269ab1278d52bbeb5b1be4e79a8668
nc1z <nc1zdev@gmail.com>
2023-01-17 11:12:40 +0800

    [Model] Created AddBookRequest Model

---
+++ server/src/main/java/com/nc1z/server/model/AddBookRequest.java
@@ -0,0 +1,19 @@
+package com.nc1z.server.model;
+
+import lombok.Data;
+
+@Data
+public class AddBookRequest {
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private int copies;
+
+    private String category;
+
+    private String img;
+}
