b230fd73d5abdd4c79bfce3d09e77be875d2bbcf
nc1z <nc1zdev@gmail.com>
2023-01-17 10:06:49 +0800

    [Controller] Updated MessagesController with PUT endpoint

--- server/src/main/java/com/nc1z/server/controller/MessagesController.java
+++ server/src/main/java/com/nc1z/server/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.nc1z.server.controller;
 
 import com.nc1z.server.entity.Message;
+import com.nc1z.server.model.AdminReplyRequest;
 import com.nc1z.server.service.MessagesService;
 import com.nc1z.server.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,15 @@ public class MessagesController {
     public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token, @RequestBody AdminReplyRequest adminReplyRequest) throws  Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Page restricted to admins only.");
+        }
+        messagesService.putMessage(adminReplyRequest, userEmail);
     }
 }
