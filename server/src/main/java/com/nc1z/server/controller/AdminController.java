b12c63345a5d20f974315589af91694a31479d9e
nc1z <nc1zdev@gmail.com>
2023-01-17 11:20:55 +0800

    [Controller] Added AdminController & POST endpoint

---
+++ server/src/main/java/com/nc1z/server/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.nc1z.server.controller;
+
+import com.nc1z.server.model.AddBookRequest;
+import com.nc1z.server.service.AdminService;
+import com.nc1z.server.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:5173/"})
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Action restricted to admins only.");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
